293612e82d6117372ac5e6b6387bd30094658b45
de-evg <d.e.min@yandex.ru>
2019-06-09 13:21:03 +0300

    поправил замечания наставника

--- js/stat.js
+++ js/stat.js
@@ -2,6 +2,9 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
+var CLOUD_SHADOW_GAP = 10;
+var CLOUD_COLOR = 'rgba(255, 255, 255, 1)';
+var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 var NAME_X = 140;
 var NAME_Y = 260;
 var TIMES_X = 140;
@@ -30,30 +33,27 @@ var anotherColor = function () {
 /**
 * Создает облако
 *
-* @constuctor
 * @param {object} ctx - контекст canvas.
-* @param {number} cloudX - координата X.
-* @param {number} cloudY - координата Y.
+* @param {number} cloudX - начальная координата X.
+* @param {number} cloudY - начальная координата Y.
 * @param {string} cloudColor - значение цвета в цветовом пространстве rgba.
 */
-var renderCloud = function (ctx, cloudX, cloudY, cloudColor) {
-  ctx.fillStyle = cloudColor;
-  ctx.fillRect(cloudX, cloudY, CLOUD_WIDTH, CLOUD_HEIGHT);
-
-  ctx.fillStyle = cloudColor;
+var renderCloud = function (ctx, cloudX, cloudY) {
+  ctx.fillStyle = SHADOW_COLOR;
+  ctx.fillRect(cloudX + CLOUD_SHADOW_GAP, cloudY + CLOUD_SHADOW_GAP, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillStyle = CLOUD_COLOR;
   ctx.fillRect(cloudX, cloudY, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
 /**
 * Создает текст в облаке
 *
-* @constuctor
 * @param {object} ctx - контекст canvas.
 * @param {string} textContent - текст, который необходимо отобразить.
 * @param {number} textX - координата X.
 * @param {number} textY - координата Y.
 */
-var renderCloudeText = function (ctx, textContent, textX, textY) {
+var renderText = function (ctx, textContent, textX, textY) {
   ctx.fillStyle = FONT_STYLE;
   ctx.font = FONT_PROPERTY;
   ctx.fillText(textContent, textX, textY);
@@ -62,31 +62,28 @@ var renderCloudeText = function (ctx, textContent, textX, textY) {
 /**
 * Создает имена игроков
 *
-* @constuctor
 * @param {object} ctx - контекст canvas.
 * @param {array} names - массив с именами игроков.
 */
 var renderPlayersNames = function (ctx, names) {
   for (var i = 0; i < names.length; i++) {
-    renderCloudeText(ctx, names[i], NAME_X + (BAR_WIDTH + GAP) * i, NAME_Y);
+    renderText(ctx, names[i], NAME_X + (BAR_WIDTH + GAP) * i, NAME_Y);
   }
 };
 
 /**
 * Создает времена прохождения для каждого игрока.
 *
-* @constuctor
 * @param {object} ctx - контекст canvas.
 * @param {array} times - массив времен для каждого из игроков.
 */
 var renderPlayersTimes = function (ctx, times) {
   var maxTime = getMaxTime(times);
   for (var i = 0; i < times.length; i++) {
-    renderCloudeText(ctx, Math.floor(times[i]), TIMES_X + (BAR_WIDTH + GAP) * i, TIMES_Y - HISTORGAM_MAX_HEIGHT * times[i] / maxTime - TIMES_VERTICAL_GAP);
+    renderText(ctx, Math.floor(times[i]), TIMES_X + (BAR_WIDTH + GAP) * i, TIMES_Y - HISTORGAM_MAX_HEIGHT * times[i] / maxTime - TIMES_VERTICAL_GAP);
   }
 };
 
-// определение максимального времени
 /**
 * Определяем максимальное время прохождения игры.
 *
@@ -104,42 +101,41 @@ var getMaxTime = function (times) {
 };
 
 /**
-* Создает гистограмму.
+* Создает гистограммы.
 *
-* @constuctor
 * @param {object} ctx - контекст canvas.
 * @param {array} names - массив с именами игроков.
 * @param {array} times - массив времен для каждого из игроков.
+* @param {number} i - номер итерации
 */
-var renderPlayerBar = function (ctx, names, times) {
+var renderPlayerBar = function (ctx, names, times, i) {
   var maxTime = getMaxTime(times);
-  for (var i = 0; i < times.length; i++) {
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = PLAYER_COLOR;
-      ctx.fillRect(BAR_X + (BAR_WIDTH + GAP) * i, BAR_Y, BAR_WIDTH, -HISTORGAM_MAX_HEIGHT * times[i] / maxTime);
-    } else {
-      ctx.fillStyle = anotherColor();
-      ctx.fillRect(BAR_X + (BAR_WIDTH + GAP) * i, BAR_Y, BAR_WIDTH, -HISTORGAM_MAX_HEIGHT * times[i] / maxTime);
-    }
+  if (names[i] === 'Вы') {
+    ctx.fillStyle = PLAYER_COLOR;
+    ctx.fillRect(BAR_X + (BAR_WIDTH + GAP) * i, BAR_Y, BAR_WIDTH, -HISTORGAM_MAX_HEIGHT * times[i] / maxTime);
+  } else {
+    ctx.fillStyle = anotherColor();
+    ctx.fillRect(BAR_X + (BAR_WIDTH + GAP) * i, BAR_Y, BAR_WIDTH, -HISTORGAM_MAX_HEIGHT * times[i] / maxTime);
   }
 };
 
 /**
 * Создает облако со статистикой прохождения игры.
 *
-* @constuctor
 * @param {object} ctx - контекст canvas.
 * @param {array} names - массив с именами игроков.
 * @param {array} times - массив времен для каждого из игроков.
 */
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, 100, 10, 'rgba(255, 255, 255, 1)');
+  renderCloud(ctx, 100, 10);
 
-  renderCloudeText(ctx, 'Ура вы победили!', 120, 40);
-  renderCloudeText(ctx, 'Список результатов:', 120, 60);
+  renderText(ctx, 'Ура вы победили!', 120, 40);
+  renderText(ctx, 'Список результатов:', 120, 60);
 
   renderPlayersNames(ctx, names);
   renderPlayersTimes(ctx, times);
-  renderPlayerBar(ctx, names, times);
+
+  for (var i = 0; i < times.length; i++) {
+    renderPlayerBar(ctx, names, times, i);
+  }
 };
